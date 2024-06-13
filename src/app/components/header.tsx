0726e25fe28aa3c262c8ea164dc414f63c951d48
Gunther <nettelgunther@outlook.es>
2024-06-12 19:30:51 -0600

    12/06/2024 ADD: Agregue el botón de cerrar sesión

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,4 +1,5 @@
 import { Divider } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import Link from "next/link";
 
 export default function Header() {
@@ -13,18 +14,26 @@ export default function Header() {
                     <h2 className="text-white text-base">José Emilio Grajales Moguel</h2>
                 </article>
             </section>
-            <nav className="h-16 bg-[#13322B]">
-                <ul className="h-full flex items-center justify-center gap-10 lg:gap-20">
-                <li>
-                    <Link href="#" className="text-white">Inicio</Link>
-                </li>
-                <li>
-                <Link href="#" className="text-white">Acerca de</Link>
-                </li>
-                <li>
-                <Link href="#" className="text-white">Contacto</Link>
-                </li>
+            <nav className="relative h-16 bg-[#13322B] flex">
+                <ul className="w-full h-full flex items-center justify-center gap-10 lg:gap-20">
+                    <li>
+                        <Link href="#" className="text-white">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="text-white">Acerca de</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="text-white">Contacto</Link>
+                    </li>
                 </ul>
+                <div className="absolute w-[1em] h-full right-36 py-2">
+                    <Divider orientation="vertical" className="bg-white mx-2" />
+                </div>
+                <div className="absolute w-[10%] h-full right-0 flex items-center justify-center">
+                    <Button className="bg-[#1d4b3f] text-white">
+                        Cerrar sesión
+                    </Button>
+                </div>
             </nav>
         </header>
     );
